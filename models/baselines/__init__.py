2916f734c338953018294a018d9d4ea782ff62c9
Parsa Alian <parsaalian0@gmail.com>
2021-09-20 10:41:58 +0430

    adding new methods and performance measures

---
+++ models/baselines/__init__.py
@@ -0,0 +1,4 @@
+from .SA_runner import *
+from .splex_runner import *
+from .random_runner import *
+from .montagne_runner import *
---
+++ utils/measures.py
@@ -0,0 +1,14 @@
+def future_performance():
+    pass
+
+
+def noise_stability():
+    pass
+
+
+def time_stability():
+    pass
+
+
+def frontier():
+    pass
