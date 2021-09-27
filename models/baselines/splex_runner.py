e3fdaa4462348854ef596110ad1c9941250f2c9b
Parsa Alian <parsaalian0@gmail.com>
2021-09-27 17:59:07 +0330

    ran splex on indextrack1

--- models/baselines/splex_runner.py
+++ models/baselines/splex_runner.py
@@ -1,5 +1,6 @@
 import numpy as np
 import networkx as nx
+from tqdm import tqdm
 
 from data import *
 from utils.readable_df import *
@@ -13,18 +14,18 @@ class SplexSolver:
         self.G = G
         self.s = s
         self.N = len(G.nodes())
+        self.nodes_list = list(G.nodes())
+        self.nodes_index = {x: i for (i, x) in enumerate(self.nodes_list)}
         self.max = 0
         self.maxi = [0 for i in range(self.N)]
         self.res = []
         self.order = []
-
 
     def w(self, X):
         res = 0
         for i in X:
             res += self.G.nodes[i]["weight"]
         return res
-
 
     def is_splex(self, X):
         Gp = self.G.subgraph(X)
@@ -33,15 +34,13 @@ class SplexSolver:
                 return False
         return True
 
-
     def min_order(self, X):
         mini, m = self.N, self.N
         for v in X:
-            if self.order.index(v) < mini:
-                mini = self.order.index(v)
+            if self.order.index(self.nodes_index[v]) < mini:
+                mini = self.order.index(self.nodes_index[v])
                 m = v
         return mini, m
-
 
     def find_max(self, C, P):
         if len(C) == 0:
@@ -62,14 +61,11 @@ class SplexSolver:
                     Cp.add(v)
             self.find_max(Cp, Pp)
 
-
     def solve(self):
-        self.order = list(np.argsort([self.G.nodes[i]["weight"] for i in self.G.nodes()]))
-        print(len(self.order))
-        for i in reversed(range(self.N)):
-            print(i)
-            C = set([self.order[x] for x in range(i + 1, self.N)])
-            self.find_max(C, {self.order[i]})
+        self.order = list(np.argsort([self.G.nodes[i]["weight"] for i in self.nodes_list]))
+        for i in tqdm(list(reversed(range(self.N)))):
+            C = set([self.nodes_list[self.order[x]] for x in range(i + 1, self.N)])
+            self.find_max(C, {self.nodes_list[self.order[i]]})
             self.maxi[i] = self.max
 
 
@@ -107,7 +103,30 @@ class SplexRunner:
         
         solver = SplexSolver(G, self.model_config.s)
         solver.solve()
-        print(solver.res)
+        
+        baskets = [list(solver.res)]
+        
+        results = []
+        
+        for i in tqdm(range(len(baskets))):
+            try:
+                assets = baskets[i]
+                weight_dict = dict(eval(self.model_config.weight_method)(train_dataset[assets], self.model_config))
+                # print(weight_dict)1
+                assets, weights = list(weight_dict.keys()), list(weight_dict.values())
+                results.append([
+                    assets,
+                    *calculate_measures(assets, train_dataset, weights)
+                ])
+            except Exception as e:
+                print(e)
+        
+        results = np.asarray(results, dtype=object)
+        df = pd.DataFrame({
+            self.output_columns[i]: results[:, i] for i in range(len(self.output_columns))
+        })
+        df = df_list_to_readable(df, ['stocks', 'weights'])
+        df.to_csv('{0}/{1}.csv'.format(self.save_dir, 'splex_results'), index=False)
     
     
     def test(self):
